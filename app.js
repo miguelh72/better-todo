2310bd54c997089471fe4dcb74249419ecb42ba1
Miguel Hernandez <miguelh72@outlook.com>
2020-07-06 13:42:08 -0400

    Added express morgan method-override nodemon and started laying out server

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+if (app.get('env') === 'development') app.use(morgan('dev'));
+app.set('view engine', 'pug');
+
+// Routing
+app.use(express.static('./public'));
+app.get('/', (req, res) => {
+	return res.render('index');
+});
+
+// Launch Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+	console.log(`
+Server running on PORT ${PORT}...
+Website live at http://localhost:${PORT}/
+`)
+);
